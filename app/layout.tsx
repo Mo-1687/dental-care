4681e0cc18e3ba8de23304de97fc4e8dbee7fb58
MO <mohamedxehab0@gmail.com>
2025-12-02 15:59:32 +0200

    set up prisma and sync user to database

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import UserSync from "@/components/UserAsync/UserAsync";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -14,10 +15,10 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-   title: "Dental Care - AI Powered Dental Assistant",
+  title: "Dental Care - AI Powered Dental Assistant",
   description:
     "Get instant dental advice through voice calls with our AI assistant. Avaiable 24/7.",
-}
+};
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -25,13 +26,14 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased dark`}
+        >
+          <UserSync />
+          {children}
+        </body>
+      </html>
     </ClerkProvider>
   );
 }
---
+++ lib/actions/doctors.ts
@@ -0,0 +1,150 @@
+"use server";
+import { toast } from "react-toastify";
+import { generateAvatar } from "../utils";
+import { revalidatePath } from "next/cache";
+import { Gender } from "@prisma/client";
+import { prisma } from "../prisma";
+
+export async function getDoctors() {
+  try {
+    const doctors = await prisma.doctors.findMany({
+      include: {
+        _count: { select: { appointment: true } },
+      },
+      orderBy: { createdAt: "desc" },
+    });
+
+    return doctors.map((doctor) => ({
+      ...doctor,
+      appointmentCount: doctor._count.appointment,
+    }));
+  } catch (error) {
+    console.log(error);
+
+    throw new Error("Failed to get doctors");
+  }
+}
+interface DoctorDataInterface {
+  name: string;
+  email: string;
+  phone: string;
+  specialty: string;
+  isActive: boolean;
+  gender: Gender;
+}
+export async function createNewDoctor(doctorData: DoctorDataInterface) {
+  try {
+    if (
+      !doctorData.name ||
+      !doctorData.email ||
+      !doctorData.phone ||
+      !doctorData.specialty
+    ) {
+      toast.error("Please fill all required fields");
+      throw new Error("Missing required fields");
+    }
+    const doctor = await prisma.doctors.create({
+      data: {
+        ...doctorData,
+        imageUrl: generateAvatar(doctorData.name, doctorData.gender),
+      },
+    });
+    revalidatePath("/admin");
+    return doctor;
+  } catch (error: { code?: string } | any) {
+    console.log(error);
+    if (error?.code === "P2002") {
+      toast.error("Doctor with this email already exists");
+      throw new Error("Doctor with this email already exists", error);
+    }
+    toast.error("Failed to create new doctor");
+    throw new Error("Failed to create new doctor", error);
+  }
+}
+
+interface EditDoctorInput extends Partial<DoctorDataInterface> {
+  id: string;
+}
+
+export async function EditDoctorData(editedData: EditDoctorInput) {
+  try {
+    if (
+      !editedData.name ||
+      !editedData.email ||
+      !editedData.phone ||
+      !editedData.specialty
+    ) {
+      toast.error("Please fill all required fields");
+      throw new Error("Missing required fields");
+    }
+    const currentDoctor = await prisma.doctors.findUnique({
+      where: {
+        id: editedData.id,
+      },
+      select: {
+        email: true,
+      },
+    });
+
+    // Is Doctor Exists
+    if (!currentDoctor) {
+      toast.error("Doctor not found");
+      throw new Error("Doctor not found");
+    }
+
+    // Check that email is unique
+    if (currentDoctor.email !== editedData.email) {
+      const existingDoctor = await prisma.doctors.findUnique({
+        where: {
+          email: editedData.email,
+        },
+      });
+
+      if (existingDoctor) {
+        toast.error("Doctor with this email already exists");
+        throw new Error("Doctor with this email already exists");
+      }
+    }
+
+    // Update Data
+    const doctor = await prisma.doctors.update({
+      where: {
+        id: editedData.id,
+      },
+      data: {
+        name: editedData.name,
+        email: editedData.email,
+        specialty: editedData.specialty,
+        isActive: editedData.isActive,
+        phone: editedData.phone,
+        gender: editedData.gender,
+      },
+    });
+    return doctor;
+  } catch (error) {
+    console.log(error); 
+    toast.error("Failed to update Doctor data");
+    throw new Error("Failed to update Doctor data");
+  }
+}
+
+export async function getAvailableDoctors() {
+  try {
+    const doctors = await prisma.doctors.findMany({
+      where: { isActive: true },
+      include: {
+        _count: {
+          select: { appointment: true },
+        },
+      },
+      orderBy: { name: "asc" },
+    });
+    return doctors.map((doctor) => ({
+      ...doctor,
+      appointmentCount: doctor._count.appointment,
+    }));
+  } catch (error) {
+    console.log(error); 
+    throw new Error("Failed to fetch available doctors:");
+  }
+}
